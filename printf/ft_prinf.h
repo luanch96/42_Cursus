10088fc1fb713c102255adc0f212217bb6f3c297
luanch96 <123073905+luanch96@users.noreply.github.com>
2023-01-19 17:01:40 +0100

    ajustre

--- printf/ft_prinf.h
+++ printf/ft_prinf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_prinf.h                                         :+:      :+:    :+:   */
+/*   ft_printf.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luisanch <luisanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:44:35 by luisanch          #+#    #+#             */
-/*   Updated: 2023/01/17 16:00:05 by luisanch         ###   ########.fr       */
+/*   Updated: 2023/01/19 16:58:33 by luisanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@
 # include <string.h>
 # include <stdlib.h> 
 # include <unistd.h>
+# include <stdarg.h>
 
-int ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
+int		ft_putchar(char c);
+void	ft_putstr(char *x);
 
 #endif
